097192517d909d1ab74b70ca359afe2d84e17ea1
anfishka <97487005+anfishka@users.noreply.github.com>
2023-06-25 23:55:53 +0300

    add solved 10th task

--- kata8.py
+++ kata8.py
@@ -93,3 +93,11 @@ def is_palindrome(s):
         return True
     else:
         return False
+
+#10th task - Convert a Boolean to a String
+
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    else:
+        return "False"
